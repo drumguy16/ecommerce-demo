9d5ae2ee3834c9e731ef03057f2f764cd62e2a05
drumguy16 <ripley.brett@gmail.com>
2016-06-06 19:14:48 -0400

    Add movie class, root path, movie resources, controller, and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :installs
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'movies#index'
+
+  resources :movies, only: [:show, :index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
