ffe7020b75dda447da2b07c3f8c4cc5f44e0401e
drumguy16 <ripley.brett@gmail.com>
2016-06-07 12:33:35 -0400

    Add devise user model and routes. Add custom styling to user forms.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root 'movies#index'
  
+  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'movies#index'
 
   resources :movies, only: [:show, :index]
 
