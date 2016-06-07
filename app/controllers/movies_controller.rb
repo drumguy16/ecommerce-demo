e1fe40e57ce3a2fa7bf4f18b430ad6956576da62
drumguy16 <ripley.brett@gmail.com>
2016-06-06 22:51:24 -0400

    Create seed file and add styling to home page.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,10 @@
 class MoviesController < ApplicationController
 	def index
+		@movies = Movie.all
+		@top_movies = @movies.take(9)
 	end
 
-	def show 
+	def show
+		@movie = Movie.find(params[:id]) 
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,24 +16,6 @@ ActiveRecord::Schema.define(version: 20160606215949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "installs", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "installs", ["email"], name: "index_installs_on_email", unique: true, using: :btree
-  add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true, using: :btree
-
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.integer  "release_year"
@@ -45,23 +27,5 @@ ActiveRecord::Schema.define(version: 20160606215949) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
-
-  create_table "views", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
-  add_index "views", ["email"], name: "index_views_on_email", unique: true, using: :btree
-  add_index "views", ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true, using: :btree
 
 end
